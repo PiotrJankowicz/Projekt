946fcbd58628f96bb0c0a26bac8034a074254aff
PiotrJankowicz <72882155+PiotrJankowicz@users.noreply.github.com>
2020-11-07 22:21:23 +0100

    Update klasa.cpp

--- klasa.cpp
+++ klasa.cpp
@@ -3,81 +3,66 @@
 
 void baza::pobierz(){
     cout<<"wprowadzam"<<endl;
-        cout << "\tpodaj imie: ";
-        cin >> this->imie;
-        cout << "\tpodaj nazwisko: ";
-        cin >> this->nazwisko;
-        cout << "\tpodaj reprezentacje: ";
-        cin >> this->kraj;
-        cout << "\tpodaj date urodzenia: ";
-        cin >> this->data;
-        cout << "\tpodaj czas reakcji: ";
+        cout << "\tPodaj sile wiatru: ";
+        cin >> this->wiatr;
+        cout << "\tPodaj czas reakcji: ";
         cin >> this->reakcja;
-        cout << "\tpodaj wynik: ";
+        cout << "\tPodaj wynik: ";
         cin >> this->wynik;
-        cout << "\tpodaj miasto: ";
-        cin >> this->miasto;
-        cout << "\tpodaj date uzyskania wyniku: ";
+        cout << "\tPodaj date uzyskania wyniku ";
         cin >> this->datawyniku;
+        cin.ignore(numeric_limits <streamsize >::max(), '\n' );
+        cout << "\tPodaj miasto: ";
+        getline(cin, miasto );
 };
 
 
 
 void baza::zapisz(fstream& plik){
     plik<<endl;
-    plik<<this->imie<<endl;
-    plik<<this->nazwisko<<endl;
-    plik<<this->kraj<<endl;
-    plik<<this->data<<endl;
+    plik<<this->wiatr<<endl;
     plik<<this->reakcja<<endl;
     plik<<this->wynik<<endl;
-    plik<<this->miasto<<endl;
     plik<<this->datawyniku<<endl;
+    plik<<this->miasto<<endl;
 };
 void baza::wczytaj(fstream& plik){
     do{
 getline(plik,lin);
-plik>>this->imie;
-getline(plik,lin);
-plik>>this->nazwisko;
-getline(plik,lin);
-plik>>this->kraj;
-getline(plik,lin);
-plik>>this->data;
+plik>>this->wiatr;
 getline(plik,lin);
 plik>>this->reakcja;
 getline(plik,lin);
 plik>>this->wynik;
 getline(plik,lin);
-plik>>this->miasto;
-getline(plik,lin);
 plik>>this->datawyniku;
+getline(plik,lin);
+plik>>this->miasto;
     }
 while(lin!="");
 };
 
 void baza::wyswietl(){
-cout<<"\tImie: "<<this->imie<<"\n";
-cout<<"\tNazwisko.: "<<this->nazwisko<<"\n";
-cout<<"\tReprezentacja.: "<<this->kraj<<"\n";
-cout<<"\tData urodzenia.: "<<this->data<<"\n";
-cout<<"\tCzas reakcji.:"<<this->reakcja<<"\n";
+cout<<"\tSila wiatru: "<<this->wiatr;
+if (wiatr>2)
+cout<<" Srednia predkosc wiatru powyzej 2 m/s. Wynik nie zalicza sie do tabel"<<"\n";
+else cout<<" Regulaminowa srednia predkosc wiatru. Wynik zalicza sie do tabel"<<"\n";
+cout<<"\tCzas reakcji:"<<this->reakcja<< "\n";
 cout<<"\tWynik: "<<this->wynik<< "\n";
+cout<<"\tData uzyskania wyniku: "<<this->datawyniku<< "\n";
 cout<<"\tmiasto: "<<this->miasto<< "\n";
-cout<<"\tdata uzyskania wyniku: "<<this->datawyniku<< "\n";
+
 };
 
 
 baza::~baza(){
 cout << "Usuwam:\n";
-cout<<"\tImie: "<<this->imie<<"\n";
-cout<<"\tNaz.: "<<this->nazwisko<<"\n";
-cout<<"\tReprezentacja.:" <<this->kraj<<"\n";
-cout<<"\tData urodzenia:" <<this->data<<"\n";
+cout<<"\tSila wiatru:" <<this->wiatr<<" m/s"<<"\n";
 cout<<"\tCzas reakcji.:"<<this->reakcja<<"\n";
 cout<<"\tWynik.: "<<this->wynik<< "\n";
-cout<<"\tmiasto.: "<<this->miasto<< "\n";
 cout<<"\tdata uzyskania wyniku.: "<<this->datawyniku<< "\n";
+cout<<"\tmiasto.: "<<this->miasto<< "\n";
 
 
 };
+
